77675d5538040887169800d6afc0bac0bfa1f297
aleja <atorresdesarrollo@gmail.com>
2024-11-27 20:30:55 +0100

    interfaz

--- TrabajoFinalPP/src/main/java/com/mycompany/trabajofinalpp/Main.java
+++ TrabajoFinalPP/src/main/java/com/mycompany/trabajofinalpp/Main.java
@@ -1,7 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-
 package com.mycompany.trabajofinalpp;
 
 import java.util.Collections;
@@ -10,27 +10,36 @@ import java.util.List;
 
 /**
  *
- * @author aleja
+ * @author alejandro
  */
-public class TrabajoFinalPP {
+public class Main {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
+        // TODO code application logic here
         List<Horno> hornosVacios = Collections.synchronizedList(new LinkedList<>());
         List<Horno> hornosLlenos = Collections.synchronizedList(new LinkedList<>());
+        List<Horno> listaHornos= Collections.synchronizedList(new LinkedList<>());
+        List<Repostero> listaReposteros= Collections.synchronizedList(new LinkedList<>());
+        List<Empaquetador> listaEmpaquetadores= Collections.synchronizedList(new LinkedList<>());
+        
         Almacen almacen= new Almacen(1000);
         for (int i = 0; i < 5; i++) {
             Horno horno= new Horno("horno"+(i+1), 200);
             Empaquetador empaquetador= new Empaquetador("empaquetador"+(i+1), hornosVacios, hornosLlenos, horno, almacen);
             hornosVacios.add(horno);
+            listaHornos.add(horno);
             horno.start();
             empaquetador.start();
         }
         Cafetera cafetera= new Cafetera();
         Repostero repostero1= new Repostero("repostero1", hornosVacios,hornosLlenos, cafetera);
         Repostero repostero2= new Repostero("repostero2", hornosVacios,hornosLlenos, cafetera);
-        
+
         repostero1.start();
         repostero2.start();
-        
     }
+    
 }
